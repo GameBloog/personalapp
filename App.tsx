598dc9cca65a3dea3baf7e5b16ffc9546cee9938
GameBloog <gimenezcavalcantepedro@gmail.com>
2024-06-29 22:56:37 -0300

    make routes

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from "@expo-google-fonts/roboto"
 import { Loading } from "@components/Loading"
 import { THEME } from "src/theme"
-import { SignIn } from "@screens/SignIn"
+import { Routes } from "./src/routes"
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   )
 }
